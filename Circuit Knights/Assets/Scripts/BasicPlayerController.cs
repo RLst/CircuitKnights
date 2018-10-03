48cc90e75b85be4838d9db51c50eafe36511b431
Tony L <letony@icloud.com>
2018-10-03 16:24:01 +1000

    Basic Player Control; Move forward/back Basic Lance Control; Tilt lance vertical/horizontal

---
+++ Circuit Knights/Assets/Scripts/BasicPlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicPlayerController : MonoBehaviour {
+	//Attached object needs a rigidbody
+
+	public float turnSpeed = 5f;
+	public float accelForce = 24000f;		//N, Roughly 1 hp
+	public float turnTorque = 1f;			//Nm
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// var rb = GetComponent<Rigidbody>();
+		// //Acceleration pulse
+		// if (Input.GetKey(KeyCode.Space))
+		// {
+		// 	rb.AddForce(transform.forward * accelForce * Time.deltaTime);
+		// }
+		// //
+	}
+
+	void FixedUpdate()
+	{
+		var rb = GetComponent<Rigidbody>();
+
+		//DEBUG CONTROLS - Turning
+		// if (Input.GetKey(KeyCode.LeftArrow))
+		// {
+		// 	transform.Rotate(0, -turnSpeed, 0);
+		// }
+		// if (Input.GetKey(KeyCode.RightArrow))
+		// {
+		// 	transform.Rotate(0, turnSpeed, 0);
+		// }
+
+
+		// var turn = Input.GetAxis("Horizontal");
+		// rb.AddTorque(transform.forward * turnTorque *turn);
+		// Debug.Log("Turn: " + turn);
+
+		//Acceleration pulse
+		if (Input.GetKey(KeyCode.W))
+		{
+			rb.AddForce(transform.forward * accelForce);
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			rb.AddForce(-transform.forward * accelForce);
+		}
+
+
+	}
+
+}
---
+++ Circuit Knights/Assets/Scripts/LanceControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanceControl : MonoBehaviour {
+
+	public float lanceHorizontalSpeed = 5;
+	public float lanceVerticalSpeed = 5;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		var rb = GetComponent<Rigidbody>();
+
+		//Vertical
+		var vertical = Input.GetAxis("Vertical");
+		transform.Rotate(0, 0, vertical);
+		
+		//Horizontal
+		var horizontal = Input.GetAxis("Horizontal");
+		transform.Rotate(-horizontal, 0, 0);
+
+	}
+}
