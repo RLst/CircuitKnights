723ac1a16574cf4af4ec895c386b056b11a9a3fe
jackdawes09 <36465192+jackdawes09@users.noreply.github.com>
2018-10-05 15:47:55 +1000

    Added CountDown Script

---
+++ Circuit Knights/Assets/Scripts/CountDown.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+//Jack Dawes and Tony Le
+//5th of October, 2018
+
+namespace CircuitKnights
+{
+
+    public class CountDown : MonoBehaviour
+    {
+
+        public Text countDownText;
+
+        public float countDownDuration = 3f;
+
+        // Use this for initialization
+        public void Start()
+        {
+            countDownText = GetComponent<Text>();
+            //this.countDownLength = GameController.countDownLength;
+        }
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (countDownDuration >= -1f)
+            {
+
+                countDownDuration -= Time.deltaTime;
+
+                countDownText.text = Mathf.Ceil(countDownDuration).ToString();
+                if (countDownDuration <= 0.0f)
+                {
+                    countDownText.text = "GO!";
+                    //Unfreeze game and play
+                }
+            }
+            else
+            {
+                countDownText.enabled = false;
+            }
+
+        }
+
+        public void StartCountDown()
+        {
+
+        }
+    }
+}
+
