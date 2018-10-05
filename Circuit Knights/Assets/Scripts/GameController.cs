48e5991bf067cd48338bd5ba48ebb4ca58cfd7d6
Tony L <letony@icloud.com>
2018-10-05 10:21:40 +1000

    Started GameController.cs; The main game script that is to essentially control and run the game

---
+++ Circuit Knights/Assets/Scripts/GameController.cs
@@ -0,0 +1,62 @@
+﻿//Tony Le
+//5 Oct 2018
+
+using System;
+using UnityEditor.UI;
+using UnityEngine;
+
+
+namespace CircuitKnights
+{
+
+public class GameController : MonoBehaviour 
+{
+
+	//[Temp]Countdown
+	public float timeBeforeCountdown = 2f; 		//Seconds
+	public float countDownLength = 3f;			//Seconds
+
+	// public Text countDownText;
+
+	public Vector3[] playerStartPositions;
+
+
+	void Update()
+	{
+		//Run GUI stuff?
+
+		//Cinematic camera pans around the stadium waiting for players to get ready
+
+		///Start the game
+		//Move players into position
+
+		//Slight delay
+		if (Time.time > timeBeforeCountdown) {
+
+			//Start the actual countdown
+			StartCountDown();
+
+
+		}
+
+
+		//Players are able to start joust run once countdown is off
+		
+
+		// RunCinematicCameras();
+	}
+
+	private void StartCountDown()
+	{
+		////NOT FINISHED!!!
+		// countDownText.Text =
+		throw new NotImplementedException();
+	}
+
+	private void RunCinematicCameras()
+	{
+		throw new NotImplementedException();
+	}
+}
+
+}
