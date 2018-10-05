f0a872e6e8f088da1cff94bb97e0cc448fa330f8
jackdawes09 <36465192+jackdawes09@users.noreply.github.com>
2018-10-05 16:04:02 +1000

    Added Exit Button Script

---
+++ Circuit Knights/Assets/Scripts/ExitButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitButton : MonoBehaviour {
+
+	public void ExitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+}
