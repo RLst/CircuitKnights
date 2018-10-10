418430056e22a2db264f833128a892da6654f6d8
jackdawes09 <36465192+jackdawes09@users.noreply.github.com>
2018-10-11 10:50:17 +1100

    Added Camera Rotation Script prototype

---
+++ Circuit Knights/Assets/Scripts/GUI/CameraRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CircuitKnights
+{
+    public class CameraRotation : MonoBehaviour
+    {
+        public GameObject Camera;
+        public Transform target;
+        public Vector3 pivot;
+        public float speed = 30.0f;
+        void Update()
+        {
+            transform.LookAt(target);
+            transform.RotateAround(pivot, Vector3.up, speed * Time.deltaTime);
+        }
+    }
+}
