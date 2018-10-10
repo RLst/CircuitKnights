24eae954f6d431884287e083806401a3c509b6f8
jackdawes09 <36465192+jackdawes09@users.noreply.github.com>
2018-10-10 16:35:56 +1100

    Added prototype for main menu/rematch button scripts

---
+++ Circuit Knights/Assets/Scripts/GUI/MainMenuButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//Jack Dawes
+//10th of October, 2018
+
+namespace CircuitKnights
+{
+
+    public class MainMenuButton : MonoBehaviour
+    {
+        public void MainMenu()
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+}
