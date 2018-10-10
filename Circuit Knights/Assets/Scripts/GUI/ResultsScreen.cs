a6b6f58628f3a5e5840f4e7283e8265e26299d6f
jackdawes09 <36465192+jackdawes09@users.noreply.github.com>
2018-10-10 16:18:56 +1100

    Added prototype of Results Screen script

---
+++ Circuit Knights/Assets/Scripts/GUI/ResultsScreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultsScreen : MonoBehaviour {
+
+    public GameObject VictoryScreen;
+    public GameObject DefeatScreen;
+
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            DefeatScreen.SetActive(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.V))
+        {
+            VictoryScreen.SetActive(true);
+        }
+        
+	}
+}
