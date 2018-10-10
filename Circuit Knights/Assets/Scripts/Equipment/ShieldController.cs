f6578571852d8f220d2d7d994c6a4d643fcdddfc
Tony L <letony@icloud.com>
2018-10-10 17:37:10 +1100

    Basic Shield controller works, but designer needs to fix player prefab

---
+++ Circuit Knights/Assets/Scripts/Equipment/ShieldController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CircuitKnights
+{
+
+public class ShieldController : MonoBehaviour {
+	////Attach this to the shield
+
+	public enum InputType {
+		LeftTrigger,
+		XButton
+	}
+
+	public InputType inputType;
+
+	public Vector3 maxBlockOffset;		//The max offset of where the shield will 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 offset;
+
+		switch (inputType) {
+			case InputType.LeftTrigger:
+				offset = maxBlockOffset * Input.GetAxis("Left Trigger");
+				transform.localPosition = offset;				
+			break;
+			case InputType.XButton:
+
+				var mult = Input.GetAxis("Fire");
+				transform.localPosition = Vector3.one * mult * Time.deltaTime;	
+			break;
+		}
+	}
+}
+
+}
