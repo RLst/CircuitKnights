b6ce38a6739fe8d3b371aea11e8c5baa82fbdc20
Tony L <letony@icloud.com>
2018-10-10 17:38:12 +1100

    Added a horse controller; physics need to be corrected

---
+++ Circuit Knights/Assets/Scripts/Player/HorseController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorseController : MonoBehaviour {
+	////Attach this to the horse
+	//Controls the movement of the horse
+	//Manual physics
+
+	public float mass = 800f;	 	//kg
+	public float linearForce = 25000f;	//Newtons
+	public float drag = 0.5f;		//Percentage?
+
+	[HideInInspector]
+	public Vector3 force;
+	[HideInInspector]
+	public Vector3 accel;
+	[HideInInspector]
+	public Vector3 vel;
+	[HideInInspector]
+	public Vector3 pos;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void Update() {
+		//Move forward
+		if (Input.GetButton("Go Forward")) {
+			Debug.Log("Space pressed");
+			AddForce(transform.forward * linearForce * Time.deltaTime);
+		}
+		// AddForce(transform.forward * linearForce * Input.GetAxis("Right Trigger") * Time.deltaTime);
+
+		//Move backward
+		if (Input.GetButton("Go Backward")) {
+			Debug.Log("B pressed");
+			AddForce(-transform.forward * linearForce * Time.deltaTime);
+		}
+		// AddForce(-transform.forward * linearForce * Input.GetAxis("Left Trigger") * Time.deltaTime);
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		//Do physics
+		DoPhysics();		
+
+		Debug.Log("force: " + force);
+		Debug.Log("accel: " + accel);
+		Debug.Log("vel: " + vel);
+	}
+
+    private void DoPhysics()
+    {
+		accel = force / mass;
+		vel = vel + accel * Time.fixedDeltaTime;
+		ApplyDrag();
+		transform.localPosition += vel * Time.fixedDeltaTime;
+    }
+
+    void AddForce(Vector3 newForce) {
+		force = newForce;
+	}
+
+	void ApplyDrag() {
+		force *= drag;
+	}
+}
