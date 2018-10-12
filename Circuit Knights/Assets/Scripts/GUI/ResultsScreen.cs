61912fad261276ce0873318c4e145b1732569278
jackdawes09 <36465192+jackdawes09@users.noreply.github.com>
2018-10-12 14:43:33 +1100

    Updated the results screen script

--- Circuit Knights/Assets/Scripts/GUI/ResultsScreen.cs
+++ Circuit Knights/Assets/Scripts/GUI/ResultsScreen.cs
@@ -1,12 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+//Jack Dawes
+//12th of October,2018
 
 public class ResultsScreen : MonoBehaviour {
 
+    public string LoadingScene = "Jack's Main";
+
     public GameObject ResultScreen;
 
-	void Update () {
+    //public Button PlayAgainButton;
+
+    public Button MainMenuButton;
+
+    private void Start()
+    {
+        //PlayAgainButton.onClick.AddListener();
+        MainMenuButton.onClick.AddListener(LoadScene);
+    }
+
+    void LoadScene()
+    {
+        SceneManager.LoadScene(LoadingScene);
+    }
+
+    void Update () {
         if (Input.GetKeyDown(KeyCode.V))
         {
             ResultScreen.SetActive(true);
