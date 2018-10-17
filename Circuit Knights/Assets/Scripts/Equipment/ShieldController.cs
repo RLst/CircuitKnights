89965a1fe89d4f2ad4d16aeceeab3311fd023d92
XavierSchneider <39175089+XavierSchneider@users.noreply.github.com>
2018-10-17 14:07:46 +1100

    Shield woprkis now :)

--- Circuit Knights/Assets/Scripts/Equipment/ShieldController.cs
+++ Circuit Knights/Assets/Scripts/Equipment/ShieldController.cs
@@ -1,43 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using XboxCtrlrInput;
 
 namespace CircuitKnights
 {
 
-public class ShieldController : MonoBehaviour {
-	////Attach this to the shield
+    public class ShieldController : MonoBehaviour
+    {
+        ////Attach this to the shield
 
-	public enum InputType {
-		LeftTrigger,
-		XButton
-	}
+        //public enum InputType {
+        //	LeftTrigger,
+        //	XButton
+        //}
+        //public InputType inputType;
 
-	public InputType inputType;
+        public XboxController controller;
 
-	public Vector3 maxBlockOffset;		//The max offset of where the shield will 
+        public XboxButton blockInput;
+        //public XboxAxis blockInput;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		Vector3 offset;
+        public Vector3 maxBlockOffset;      //The max offset of where the shield will 
 
-		switch (inputType) {
-			case InputType.LeftTrigger:
-				offset = maxBlockOffset * Input.GetAxis("Left Trigger");
-				transform.localPosition = offset;				
-			break;
-			case InputType.XButton:
 
-				var mult = Input.GetAxis("Fire");
-				transform.localPosition = Vector3.one * mult * Time.deltaTime;	
-			break;
-		}
-	}
-}
+        // Update is called once per frame
+        void Update()
+        {
+            Vector3 offset = Vector3.zero; 
+            
+            if (XCI.GetButton(blockInput, controller))
+            {
+                offset = maxBlockOffset;
+                //offset = maxBlockOffset * XCI.GetAxis(blockInput, controller);
 
+            }
+
+            transform.localPosition = offset;
+
+        }
+    }
 }
