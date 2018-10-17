92d832f886d986bfe482c75865b70ec67ada0096
Tony Le <letony@icloud.com>
2018-10-17 12:49:00 +1000

    Transfer commit from MBP Halfway through rewriting horse controller and implementing a new player controller (lean/dodge)

--- Circuit Knights/Assets/Scripts/Player/HorseController.cs
+++ Circuit Knights/Assets/Scripts/Player/HorseController.cs
@@ -1,16 +1,27 @@
-﻿using UnityEngine;
+﻿//Tony Le
+//14 Oct 2018
+
+using UnityEngine;
 using XboxCtrlrInput;
 
 public class HorseController : MonoBehaviour {
-	////(Should be) attached to the horse
+	////(Should be) attached to the horse (or parent of the horse with correct transform)
 	//Controls the movement of the horse
 	//Manual physics
 
+	private Rigidbody rb;
+
+	[Header("A")]
+
 	public float mass = 800f;	 	//kg
 	public float linearForce = 25000f;	//Newtons
-	public float drag = 0.5f;		//Percentage?
+	public float drag = 4f;			//Newtons (probably)
 
+
+	[Header("Gamepad Controls")]
 	public XboxController controller;
+	public XboxAxis forward;
+	public XboxAxis backward;
 
 	[HideInInspector] public Vector3 force;
 	[HideInInspector] public Vector3 accel;
@@ -19,41 +30,58 @@ public class HorseController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody>();
+		rb.drag = this.drag;
+		rb.mass = this.mass;
 	}
+
+	void FixedUpdate() {
+		//Get controller inputs
+		var fwd = XCI.GetAxis(forward, controller);
+		var back = -XCI.GetAxis(backward, controller);
+
+		var combined = fwd + back;
+
+		//Add forward force
+		rb.AddForce(transform.forward * linearForce * combined);
+
+		//If the end is reached
+	}
+
 	
-	void Update() {
-
-		//Move forward
-		var forward = XCI.GetAxis(XboxAxis.RightTrigger, controller);
-		var backward = XCI.GetAxis(XboxAxis.LeftTrigger, controller);
-		AddForce(transform.forward * linearForce * forward * Time.deltaTime);
-		AddForce(-transform.forward * linearForce * backward * Time.deltaTime);
-
-	}
-
-	// Update is called once per frame
-	void FixedUpdate () {
-		
-		//Do physics
-		DoPhysics();		
-
-		Debug.Log("force: "+force + ", accel: "+accel + ", vel: "+vel);
-	}
-
-    private void DoPhysics()
-    {
-		accel = force / mass;
-		vel = vel + accel * Time.fixedDeltaTime;
-		ApplyDrag();
-		transform.localPosition += vel * Time.fixedDeltaTime;
-    }
-
-    void AddForce(Vector3 newForce) {
-		force = newForce;
-	}
-
-	void ApplyDrag() {
-		force *= drag;
-	}
 }
+
+
+// 	void Update() {
+// 		//Move forward
+// 		var forward = XCI.GetAxis(XboxAxis.RightTrigger, controller);
+// 		var backward = XCI.GetAxis(XboxAxis.LeftTrigger, controller);
+// 		AddForce(transform.forward * linearForce * forward * Time.deltaTime);
+// 		AddForce(-transform.forward * linearForce * backward * Time.deltaTime);
+// 	}
+
+// 	// Update is called once per frame
+// 	void FixedUpdate () {
+		
+// 		//Do physics
+// 		DoPhysics();
+
+// 		Debug.Log("force: "+force + ", accel: "+accel + ", vel: "+vel);
+// 	}
+
+//     private void DoPhysics()
+//     {
+// 		accel = force / mass;
+// 		vel = vel + accel * Time.fixedDeltaTime;
+// 		ApplyDrag();
+// 		transform.localPosition += vel * Time.fixedDeltaTime;
+//     }
+
+//     void AddForce(Vector3 newForce) {
+// 		force = newForce;
+// 	}
+
+// 	void ApplyDrag() {
+// 		AddForce(-force * drag);
+// 	}
+// }
---
+++ Circuit Knights/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	[Header("Temp")]
+	public Rigidbody rb;
+
+	public float Ltorque = 50f;		//Linear torque
+	
+	public float smoothness = 5f;		//Lerp back to zero
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+		//Constantly apply torque to "seek" toward centre
+
+		//Handle lean
+		Lean();
+
+		// TestRotate();
+	}
+
+	private void TestRotate() {
+		if (Input.GetKey(KeyCode.X))
+			transform.Rotate(10, 0, 0);
+	}
+    private void Lean()
+    {
+		//Forward and back
+		if (Input.GetKey(KeyCode.W)) {
+			rb.AddRelativeTorque(-transform.forward * Ltorque);
+		}
+		if (Input.GetKey(KeyCode.S)) {
+			rb.AddRelativeTorque(transform.forward * Ltorque);
+		}
+		//Left and right
+		if (Input.GetKey(KeyCode.A)) {
+			rb.AddRelativeTorque(transform.right * Ltorque);
+		}    
+		if (Input.GetKey(KeyCode.D)) {
+			rb.AddRelativeTorque(-transform.right * Ltorque);
+		}
+
+		///Lerp back to centre
+		//Get current rotations
+		var currentRotation = transform.rotation;
+		var targetRotation = Quaternion.identity;
+
+		transform.rotation = Quaternion.Lerp(currentRotation, targetRotation, smoothness * Time.fixedDeltaTime);
+
+
+		// var XA = transform.rotation.x;
+		// var ZA = transform.rotation.z;
+		// Debug.Log("XA: "+XA + " ZA: "+ZA);
+
+		// var curAngle = new Vector3(XA, 0f, ZA);
+		// var tarAngle = new Vector3(0,0,0);
+
+		// var lerpedRotation = Vector3.Lerp(curAngle, tarAngle, smoothness);
+		// transform.Rotate(lerpedRotation);
+
+
+		//Move back to centre
+		// var testReturnTorqueMultiplier = 100f;
+		// var xAxisReturnTorque = -transform.rotation.x * testReturnTorqueMultiplier;
+		// var zAxisReturnTorque = -transform.rotation.z * testReturnTorqueMultiplier;
+		// // rb.AddRelativeTorque(xAxisReturnTorque, 0f, zAxisReturnTorque);
+		// transform.Rotate(xAxisReturnTorque, 0, zAxisReturnTorque);
+
+	}
+}
