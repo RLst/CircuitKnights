e40b72e949442c0f53a88980d5b766a5cdc1afa7
brentdauria <38484978+brentdauria@users.noreply.github.com>
2018-10-18 14:46:58 +1100

    Adding credits

---
+++ Circuit Knights/Assets/Scripts/GUI/CreditsMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsMenu : MonoBehaviour {
+    public Animator textAnimator;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public void onClick () {
+        textAnimator.SetTrigger("startcreditroll");
+		
+	}
+}
