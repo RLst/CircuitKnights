22642a34dff8c46645b1b7994923f1bff59ab45b
Tony Le <letony@icloud.com>
2018-10-18 16:02:20 +1000

    Refined and cleaned up Lance Control script

--- Circuit Knights/Assets/Scripts/Equipment/LanceControl.cs
+++ Circuit Knights/Assets/Scripts/Equipment/LanceControl.cs
@@ -8,10 +8,6 @@ using XboxCtrlrInput;
 
 namespace CircuitKnights {
 
-public enum LerpMode {
-	lerp,
-	slerp
-}
 
 public class LanceControl : MonoBehaviour {
 
@@ -21,22 +17,18 @@ public class LanceControl : MonoBehaviour {
 	public XboxAxis horizontal = XboxAxis.RightStickX;		//for yaw
 
 	[Header("Lance Physics")]
-	public float momentOfInertia = 2f;		//kg.m2
-	public float pitchTorque = 10000;
-	public float yawTorque = 10000;
+	//Some of these should be split up into a separate Lance.cs scriptable objects
+	public float mass = 20f;		//kg			
+	public float length = 3.3f;		//metres
+	float momentOfInertia;		//kg.m2
+	public float pitchTorque = 40000;
+	public float yawTorque = 40000;
 	Vector3 angAccel;
 	Vector3 angVel;
 	Vector3 angPos;
-	public float angDrag = 1.1f;		//2 = half
-	public float angDragThreshold = 0.01f;
+	public float angDrag = 1.065f;		//2 = half
 
-	[Header("Lance Properties [PHASING OUT]")]
-	public LerpMode lerpMode = LerpMode.slerp;
-	public float weighting = 0.02f;
-	public float pitchSpeed = 80f;
-	public float yawSpeed = 100f;
-
-	[Header("Lance Limits [WORKINPROGRESS]")]
+	[Header("Lance Limits")]
 	public float minPitchAngle = -10f;
 	public float maxPitchAngle = 50f;
 	public float minYawAngle = 120f;
@@ -45,7 +37,10 @@ public class LanceControl : MonoBehaviour {
 
 	void Start() {
 		//Set the initial lance orientation
-		angPos = transform.eulerAngles;
+		angPos = transform.localRotation.eulerAngles;
+
+		//Calculate the lance's moment of inertia
+		momentOfInertia = 1f / 3f * mass * length * length;
 	}
 
 	void Update()
@@ -55,11 +50,12 @@ public class LanceControl : MonoBehaviour {
 		//Get controller inputs
 		var v = XCI.GetAxisRaw(vertical, controller);
 		var h = XCI.GetAxisRaw(horizontal, controller);
-		Debug.Log("v: "+v + " h: "+h);
+		Debug.Log("vertical: "+v + " horizonal: "+h);
 
 		//Calc angular accel
 		angAccel.x += v * pitchTorque / momentOfInertia * deltaTime;
 		angAccel.y += h * yawTorque / momentOfInertia * deltaTime;
+		Debug.Log("MOI: "+momentOfInertia);
 		Debug.Log("angAccel: " + angAccel);
 
 		//Calc angular vel
@@ -71,10 +67,9 @@ public class LanceControl : MonoBehaviour {
 		angPos.x += angVel.x * deltaTime;
 		angPos.y += angVel.y * deltaTime;
 		Debug.Log("angPos: " + angPos);
-	
-		transform.localRotation = Quaternion.Euler(angPos);
 
 		//Apply drag by reducing the accel and vel
+		angDrag = Mathf.Clamp(angDrag, 1f, 10f);
 		angAccel = angAccel / angDrag;
 		angVel /= angDrag;
 
@@ -83,79 +78,8 @@ public class LanceControl : MonoBehaviour {
 		angPos.y = Mathf.Clamp(angPos.y, minYawAngle, maxYawAngle);
 		angPos.z = 0f;
 
-
-		//Richard's suggestion
-		//Add velocity to rotation
-		//drag
-		//clamp
-
-
-		//Add delta roation (based on input) to velocity
-		// Vector3 rot = transform.eulerAngles;
-		// rot.x += v * deltaTime * yawSpeed;
-		// rot.y += h * deltaTime * pitchSpeed;
-		
-
-		// transform.rotation = Quaternion.Euler(rot);
-
-		//Lance target rotation
-		// tarAng *= Quaternion.Euler(-h * yawSpeed * deltaTime, 0f, v * pitchSpeed * deltaTime);			////!!!! Need the artists to fix the lance's pivot rotation
-		//tarAng *= Quaternion.Euler(v * pitchSpeed * deltaTime, h * yawSpeed * deltaTime, 0f);
-
-		////Temp - Revision
-		/*
-		//Linear motion
-		force = mass * accel;
-		accel = force / mass;
-		vel = vel + accel * dt;
-		pos = pos + vel * dt;
-
-		//Rotational motion
-		torque = momentOfInertia  * angAccel <=> 
-		angAccel = torque / momentOfInertia;
-		angVel = angVel + angAccel * dt;
-		angPos = angPos + angVel * dt;
-
-		 */
-
-		// angDrag = Mathf.Clamp(angDrag, 0.01f, 1000f);
-		// if (angAccel.x != 0f)
-		// {
-		// 	angAccel.x /= angDrag;
-		// 	if (angAccel.x < angDragThreshold)
-		// 		angAccel.x = 0f;
-		// }
-		// if (angAccel.y != 0f) {
-		// 	angAccel.y /= angDrag;
-		// 	if (angAccel.y < angDragThreshold)
-		// 		angAccel.y = 0f;
-		// }
-		// if (angVel.x != 0f) {
-		// 	angVel.x = angVel.x / angDrag;
-		// 	if (angVel.x < angDragThreshold)
-		// 		angVel.x = 0f;
-		// }
-		// if (angVel.y != 0f) {
-		// 	angVel.y /= angDrag;
-		// 	if (angVel.y < angDragThreshold)
-		// 		angVel.y = 0f;
-		// }
-		// Debug.Log("angDrag: "+angDrag);
-
-		//Lerp the lance
-		//switch (lerpMode) 
-		//{
-		//	case LerpMode.lerp: transform.rotation = Quaternion.Lerp(transform.rotation, tarAng, weighting); break;
-		//	case LerpMode.slerp: transform.rotation = Quaternion.Slerp(transform.rotation, tarAng, weighting); break;
-		//}
-
-		//Limit the lance [WORK IN PROGRESS]
-		// var yawAngClamp = Mathf.Clamp(transform.rotation., Quaternion.)
-		// var pitchAngClamp
-		//  = Mathf.Clamp(transform.eulerAngles.z, minPitchAngle, maxPitchAngle); 
-		//  = Mathf.Clamp(transform.eulerAngles.x, minYawAngle, maxYawAngle);
-		// transform.rotation.eulerAngles.Set(yawAngClamp, 0f, pitchAngClamp);
-
+		//Apply rotation
+		transform.localRotation = Quaternion.Euler(angPos);
 	}
 
 }
