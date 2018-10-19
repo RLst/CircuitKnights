1d4034c33ff71458cf5b83674b3234f01dd6c81d
jackdawes09 <36465192+jackdawes09@users.noreply.github.com>
2018-10-19 12:30:06 +1100

    Created horse reset script

---
+++ Circuit Knights/Assets/Scripts/Core/HorseReset.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CircuitKnights
+{
+    public class HorseReset : MonoBehaviour
+    {
+        [SerializeField] Transform horseStartPosition;
+
+        private GameObject[] resetTrigger;
+
+        private void Start()
+        {
+            resetTrigger = GameObject.FindGameObjectsWithTag("Reset");
+        }
+
+        private void OnTriggerEnter()
+        {
+            //Do things
+
+
+        }
+
+    }
+}
