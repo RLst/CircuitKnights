a88d213d592483e4564c01b17f5daea37865564a
jackdawes09 <36465192+jackdawes09@users.noreply.github.com>
2018-10-26 10:01:11 +1100

    added Robot floating script

---
+++ Circuit Knights/Assets/Scripts/GUI/RobotFloat.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Jack Dawes
+//26th of October, 2018
+
+namespace CircuitKnights
+{
+    public class RobotFloat : MonoBehaviour
+    {
+        [SerializeField] float FloatStrength = 1;
+        float OriginalY;
+        void Start()
+        {
+            this.OriginalY = this.transform.position.y;
+        }
+
+        void Update()
+        {
+            transform.position = new Vector3(transform.position.x,
+            OriginalY + ((float)Mathf.Sin(Time.time) * FloatStrength),
+            transform.position.z);
+        }
+    }
+}
