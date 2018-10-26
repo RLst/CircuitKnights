640f3b2885882d4b8c722489d44be0474ac7a3b6
Tony Le <letony@icloud.com>
2018-10-26 15:47:28 +1000

    EOD commit

--- Circuit Knights/Assets/Scripts/Scriptables/Events/GameEventListener.cs
+++ Circuit Knights/Assets/Scripts/Scriptables/Events/GameEventListener.cs
@@ -9,7 +9,7 @@ namespace CircuitKnights.Events
 {
 
 public class GameEventListener : MonoBehaviour {
-	[Tooltip("Event ot register with")] public GameEvent Event;
+	[Tooltip("Event to register with")] public GameEvent Event;
 	[Tooltip("Response to invoke when Event is raised")] public UnityEvent Response;
 
 	private void OnEnable()
