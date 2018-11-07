6720d1058f51810244e41952cdb390ba007aba44
Tony Le <letony@icloud.com>
2018-11-08 10:07:35 +1100

    HorseMover.cs renamed to PlayerMover.cs

---
+++ Circuit Knights/Assets/Scripts/Player/PlayerMover.cs
@@ -0,0 +1,70 @@
+﻿//DuckBike
+//Tony Le
+//31 Oct 2018
+
+using UnityEngine;
+using CircuitKnights.Objects;
+using UnityEngine.Assertions;
+
+namespace CircuitKnights
+{
+    public class PlayerMover : MonoBehaviour
+    {
+		[TextArea] [SerializeField] string description = "Handles horse movement ONLY";
+		[SerializeField] Horse horse;
+		[SerializeField] Player player;
+		private PlayerInput playerInput;
+		Vector3 startPosition;
+		Vector3 tarPos;
+
+		void Awake()
+		{
+			//Used to poll the controller axes
+			playerInput = GetComponentInParent<PlayerInput>();
+			if (!playerInput)
+				playerInput = GetComponent<PlayerInput>();
+
+			//Remember the initial starting position
+			startPosition = transform.position;
+			tarPos = transform.position;
+		}
+
+		void Start()
+		{
+			Assert.IsNotNull(playerInput, "Input not found!");
+		}
+
+		void FixedUpdate()
+		{
+			MoveByLerp();
+		}
+
+		void MoveByLerp()
+		{
+			//Adjust the target position
+			tarPos += transform.forward * horse.speed * playerInput.AccelAxis * Time.deltaTime;
+
+			//Clamp lerp
+			// horse.ClampTValue();
+
+			//Lerp towards it
+			transform.position = Vector3.Lerp(transform.position, tarPos, horse.lerpSmoothness);
+		}
+
+		public void SetDesiredPosition(Vector3 desiredPos)
+		{
+			//Used after a pass has occurred
+			tarPos = desiredPos;
+		}
+
+		public void SetPosition(Vector3 position)
+		{
+			//Used to instantly position the object ie. To reset the positions etc
+			transform.position = position;
+			tarPos = position;
+		}
+
+
+    }
+
+}
