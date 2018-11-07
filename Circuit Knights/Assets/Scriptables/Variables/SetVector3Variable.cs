949251095a9f72691e5804112ad33c901073a47c
Tony Le <letony@icloud.com>
2018-11-08 09:52:26 +1100

    Added SetVector3; Allows designer to set a vector 3 variable to an object’s position, euler rotation or local scale

---
+++ Circuit Knights/Assets/Scriptables/Variables/SetVector3Variable.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace CircuitKnights.Variables
+{
+    public class SetVector3Variable : MonoBehaviour
+    {
+        //Sets a Vector3 variable to either position, rotate or scale
+        [SerializeField] Vector3Variable variable;
+        public enum TransformType
+        {
+            Position,
+            Rotation,
+            Scale
+        }
+        [SerializeField] TransformType transformType;
+
+        void Awake()
+        {
+            if (variable)
+            {
+                switch (transformType)
+                {
+                    case TransformType.Position:
+                        variable.Value = this.transform.position;
+                        break;
+                    case TransformType.Rotation:
+                        variable.Value = this.transform.rotation.eulerAngles;
+                        break;
+                    case TransformType.Scale:
+                        variable.Value = this.transform.localScale;
+                        break;
+                    default:
+                        Debug.LogError("Transform type not selected!");
+                        break;
+                }
+            }
+			else
+				Debug.LogError("Vector3 variable not set!");
+        }
+    }
+}
