8c4e13caf47de5c237ee1dac3216fe6ff8747b78
Tony Le <letony@icloud.com>
2018-11-08 10:16:34 +1100

    Added simple script to draw a some gizmos in editor only to help with position objects

---
+++ Circuit Knights/Assets/Scripts/Core/DrawGizmo.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace CircuitKnights
+{
+    public class DrawGizmo : MonoBehaviour
+    {
+				public enum GizmoType {
+					Cube,
+					Line,
+					Sphere,
+					CubeWireframe,
+					SphereWireframe
+				}
+
+				[SerializeField] Color color = Color.red;
+				[SerializeField] GizmoType gizmoType = GizmoType.Cube;
+				[SerializeField] Vector3 scale = Vector3.one;
+
+				void OnDrawGizmos()
+				{
+					Gizmos.color = color;
+					switch (gizmoType)
+					{
+						case GizmoType.Cube:
+							Gizmos.DrawCube(transform.position, scale);
+						break;
+						case GizmoType.Line:
+							Gizmos.DrawLine(transform.position, transform.position + transform.localRotation.eulerAngles.normalized * 1f);
+						break;
+					}
+				}
+    }
+
+}
