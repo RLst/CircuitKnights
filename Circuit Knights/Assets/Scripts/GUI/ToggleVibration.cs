588a310230f2233678296868b9e5629bca2562cc
Tony Le <letony@icloud.com>
2018-11-08 11:59:33 +1100

    Basic implementation of script to toggle vibration functionality

---
+++ Circuit Knights/Assets/Scripts/GUI/ToggleVibration.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using CircuitKnights.Variables;
+
+namespace CircuitKnights
+{
+    public class ToggleVibration : MonoBehaviour
+    {
+        [TextArea][SerializeField] string description = 
+            "Attach this to the toggle button. Invoke Toggle() using the toggle button's unity event.";
+        [SerializeField] BoolVariable vibration;    //This CK Variable can be reference by any object that needs it ie. PlayerInput, DamageDealer
+        public void Toggle()    //Just make the vibration toggle button invoke this using it's unityevent system
+        {
+            vibration.Value = !vibration.Value;
+        }
+    }
+}
