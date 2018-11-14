b3ca6f10005abe24ab40a428be8a6cd0732cfd9e
jackdawes09 <36465192+jackdawes09@users.noreply.github.com>
2018-11-15 10:06:40 +1100

    updated time controller script

--- Circuit Knights/Assets/Scripts/Core/TimeController.cs
+++ Circuit Knights/Assets/Scripts/Core/TimeController.cs
@@ -5,54 +5,88 @@
 using UnityEngine;
 using UnityEngine.Assertions;
 using CircuitKnights.Variables;
+using System;
+using System.Collections;
 
 namespace CircuitKnights
 {
     public class TimeController : MonoBehaviour
     {
-        [SerializeField] float slowdownFactor = 0.15f;
-        [SerializeField] float slowdownDuration = 2.5f;
-        [SerializeField] float range = 30f;
+        [SerializeField] float slowdownFactor = 0.05f;
+        [SerializeField] float slowMotionDuration = 2.0f;
 
-        [SerializeField] float idealFPS = 60f;
-
-        [Header("Players")]
-        [SerializeField] TransformVariable playerOne;
-        [SerializeField] TransformVariable playerTwo;
-
-        void Start()
-        {
-            Assert.IsNotNull(playerOne, "Player one transform not found!");
-            Assert.IsNotNull(playerTwo, "Player two transform not found!");
-        }
-
+        bool toggleSlowMo = false;
         void Update()
         {
-            if (Time.timeScale < 1f)   //Slight optimization
+            //DEBUG
+            if (Input.GetKeyDown("s"))
             {
-                Time.timeScale += (1f / slowdownDuration) * Time.unscaledDeltaTime;
-            }
-
-            //Calc distance
-            var dist = Vector3.Distance(playerOne.Value.position, playerTwo.Value.position);
-            var playerOneFacing = playerOne.Value.TransformDirection(Vector3.forward);
-            var toPlayerTwo = playerTwo.Value.position - playerOne.Value.position;
-
-            //If the players are facing each other...
-            if (Vector3.Dot(playerOneFacing, toPlayerTwo) > 0)
-            {
-                //and within range
-                if (dist <= range)
+                if (Input.GetKey(KeyCode.LeftShift))
                 {
-                    SlowMotion();
+                    Debug.Log("slowmo2");
+                    SlowMotionOn(slowMotionDuration);
+                }
+                else
+                {
+                    toggleSlowMo = !toggleSlowMo;
+                    if (toggleSlowMo)
+                        SlowMotionOn();
+                    else
+                        SlowMotionOff();
                 }
             }
         }
 
-        public void SlowMotion()
+        public void SlowMotionOn()
         {
             Time.timeScale = slowdownFactor;
-            Time.fixedDeltaTime = Time.timeScale * 1f / idealFPS;
+            Time.fixedDeltaTime = Time.timeScale * 0.02f;
+        }
+
+        public void SlowMotionOn(float transitionOutDuration)
+        {
+            StartCoroutine(TransitionBackToRealtime(transitionOutDuration));
+        }
+
+        private IEnumerator TransitionBackToRealtime(float transitionOutDuration)
+        {
+            Time.timeScale = slowdownFactor;
+            while (Time.timeScale < 1f)
+            {
+                Time.timeScale += (1f / transitionOutDuration) * Time.unscaledDeltaTime;
+                Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
+                yield return null;
+            }
+            // Time.timeScale = 1f;
+            // yield return new WaitUntil(() => Time.timeScale >= 1f);
+        }
+
+        public void SlowMotionOff()
+        {
+            Time.timeScale = 1f;
+            Time.fixedDeltaTime = 1f;
         }
     }
 }
+
+
+
+// if (Time.timeScale < 1f)   //Slight optimization
+// {
+//     Time.timeScale += (1f / slowdownDuration) * Time.unscaledDeltaTime;
+// }
+
+// //Calc distance
+// var dist = Vector3.Distance(playerOne.Value.position, playerTwo.Value.position);
+// var playerOneFacing = playerOne.Value.TransformDirection(Vector3.forward);
+// var toPlayerTwo = playerTwo.Value.position - playerOne.Value.position;
+
+// //If the players are facing each other...
+// if (Vector3.Dot(playerOneFacing, toPlayerTwo) > 0)
+// {
+//     //and within range
+//     if (dist <= range)
+//     {
+//         SlowMotion();
+//     }
+// }
