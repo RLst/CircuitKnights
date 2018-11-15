f9a2e30b82f293db05fc3dec578d1d979b5b9737
brentdauria <38484978+brentdauria@users.noreply.github.com>
2018-11-15 15:10:27 +1100

    Adding vibration

---
+++ Circuit Knights/Assets/Scripts/Core/SetVibrationHandler.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using CircuitKnights.Variables;
+
+public class SetVibrationHandler : MonoBehaviour {
+
+    [SerializeField] BoolVariable isVibration;
+
+
+   public void SetVibration(bool setVibration)
+    {
+        isVibration.Value = setVibration;
+    }
+}
---
+++ Circuit Knights/Assets/Scripts/Equipment/LanceOnImpact.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CircuitKnights.Variables;
+//Brent D'Auria
+
+public class LanceOnImpact : MonoBehaviour {
+
+    [SerializeField] BoolVariable isVibration;
+    [SerializeField] float LeftMotor = .5f;
+    [SerializeField] float RightMotor = .5f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player1")
+        {
+            VibrateOnCollision(XInputDotNetPure.PlayerIndex.One);
+        }
+        
+    }
+
+    void VibrateOnCollision(XInputDotNetPure.PlayerIndex playerIndex)
+    {
+        if (isVibration)
+        {
+            XInputDotNetPure.GamePad.SetVibration(playerIndex, LeftMotor, RightMotor);
+        }
+    }
+}
