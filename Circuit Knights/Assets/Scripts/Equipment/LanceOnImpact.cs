c443ba2ffe7aab338a2b8113e774f12fd055d152
brentdauria <38484978+brentdauria@users.noreply.github.com>
2018-11-15 16:59:22 +1100

    finished vibration and mad sure it works when vibration is turned off in the menu

--- Circuit Knights/Assets/Scripts/Equipment/LanceOnImpact.cs
+++ Circuit Knights/Assets/Scripts/Equipment/LanceOnImpact.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 using CircuitKnights.Variables;
+using CircuitKnights;
+using CircuitKnights.Objects;
+using XInputDotNetPure;
+
 //Brent D'Auria
 
 public class LanceOnImpact : MonoBehaviour {
@@ -9,25 +13,50 @@ public class LanceOnImpact : MonoBehaviour {
     [SerializeField] BoolVariable isVibration;
     [SerializeField] float LeftMotor = .5f;
     [SerializeField] float RightMotor = .5f;
+    public GameObject player;
+    // PlayerData playerData;
+    //
+    // private void Awake()
+    // {
+    //     playerData = GetComponentInParent<Player>().Data;
+    // }
+    //
 
+    private void Update()
+    {
+        
+    }
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Player1")
+        //checks if the lance is hiting the player 
+       if (collision.gameObject == player)
         {
+            //resets the vibration on enter
+                LeftMotor = .5f;
+                RightMotor = .5f;
+            //selects what controlers to vibrate
+                VibrateOnCollision(XInputDotNetPure.PlayerIndex.One);
+                Debug.Log("vibrating");
+                VibrateOnCollision(XInputDotNetPure.PlayerIndex.Two);
+            //VibrateOnCollision((PlayerIndex)playerData.No as PlayerIndex);
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject == player)
+        {
+            // changes the values of the vibratio so it stops vibrating
+            LeftMotor = .0f;
+            RightMotor = .0f;
             VibrateOnCollision(XInputDotNetPure.PlayerIndex.One);
             Debug.Log("vibrating");
-        }
-        if (collision.gameObject.tag == "Player2")
-        {
             VibrateOnCollision(XInputDotNetPure.PlayerIndex.Two);
-            Debug.Log("vibrating");
         }
-
     }
 
     void VibrateOnCollision(XInputDotNetPure.PlayerIndex playerIndex)
     {
-        if (isVibration)
+        if (isVibration.Value == true)
         {
             XInputDotNetPure.GamePad.SetVibration(playerIndex, LeftMotor, RightMotor);
         }
