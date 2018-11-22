96edd460d0292d05b062cb7c09dbc2d46635412a
jackdawes09 <36465192+jackdawes09@users.noreply.github.com>
2018-11-23 09:57:47 +1100

    Added RandomClashSound script for lance clash sounds

---
+++ Circuit Knights/Assets/Scripts/Core/RandomClashSound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomClashSound : MonoBehaviour {
+
+	public AudioSource _as;
+	public AudioClip[] audioClipArray;
+
+	void Awake () {
+		_as = GetComponent<AudioSource> ();
+	}
+	
+	
+	void Start () {
+		_as.clip = audioClipArray[Random.Range(0,audioClipArray.Length)];
+		_as.PlayOneShot (_as.clip);
+	}
+}
