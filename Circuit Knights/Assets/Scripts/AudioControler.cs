176bc8d259e3f7fe8861759a42fef8a5f813e7e4
brentdauria <38484978+brentdauria@users.noreply.github.com>
2018-11-28 16:01:34 +1100

    add scene

---
+++ Circuit Knights/Assets/Scripts/AudioControler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioControler : MonoBehaviour {
+
+    public AudioSource CheeringAS;
+    public AudioClip CheeringAudioClip;
+
+    public AudioSource RobotAS;
+    public AudioClip RobotAudioClip;
+
+    public GameObject player;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == player)
+        {
+            CheeringAS.clip = CheeringAudioClip;
+            CheeringAS.PlayOneShot(CheeringAS.clip);
+
+            // _asTwo.clip = audioClip;
+            // _asTwo.PlayOneShot (_asTwo.clip);                  
+        }
+    }
+
+}
