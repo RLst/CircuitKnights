5610e77d33a6d118cd4912a9b9f4beff54f763a1
Tony Le <letony@icloud.com>
2018-11-30 04:02:06 +1100

    Light vibration when horse is moving

---
+++ Circuit Knights/Assets/Scripts/Player/HorseVibration.cs
@@ -0,0 +1,40 @@
+﻿//DuckBike
+//Tony Le
+//30 Nov 2019
+
+using UnityEngine;
+using CircuitKnights.Controllers;
+using CircuitKnights.Objects;
+
+namespace CircuitKnights
+{
+    public class HorseVibration : MonoBehaviour
+    {
+        VibrationController vibrationController;
+        Horse horse;
+        Player player;
+
+        [SerializeField] float minVibration = 0f;
+        [SerializeField] float maxVibration = 1f;
+
+
+
+        private void Awake() {
+            vibrationController = VibrationController.Instance;
+            horse = GetComponent<Horse>();
+            player = GetComponent<Player>();
+        }
+
+        private void Update() {
+            //Constantly poll the horse's speed and set vibration accordingly
+            var horseMotorVibration = Map(horse.Vel.magnitude, horse.MinSpeed, horse.MaxSpeed, minVibration, maxVibration);
+            vibrationController.VibrateOn(player.Data.No, 0f, horseMotorVibration);
+        }
+
+
+        float Map(float value, float valueMin, float valueMax, float mappedMin, float mappedMax)
+        {
+            return ((value - valueMin) / (valueMax - valueMin)) * (mappedMax - mappedMin) + mappedMin;
+        }
+    }
+}
