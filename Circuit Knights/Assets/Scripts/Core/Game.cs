f962f0b6dd2819810af6b0f63bffeeb5ea7e2e54
RLst <letony@icloud.com>
2018-12-07 17:32:48 +1100

    Implementing InitializeGame(), RunIntro(), BeginNewround()

--- Circuit Knights/Assets/Scripts/Core/Game.cs
+++ Circuit Knights/Assets/Scripts/Core/Game.cs
@@ -1,22 +1,26 @@
-﻿using System;
+﻿//Tony Le
+//6 Dec 2018
+
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using CircuitKnights.Players;
 using UnityEngine;
+using UnityEngine.UI;
+using XboxCtrlrInput;
 
 namespace CircuitKnights.Controllers
 {
     public class Game : MonoBehaviour
     {
-#region Init stuff
+    #region Init stuff
 
 		////Singleton
 
 		////Players
-		[SerializeField] Player p1;
-		[SerializeField] Player p2;
-		public Player P1 { get { return p1; } }
-		public Player P2 { get { return p2; } }
+        [SerializeField] Player p1;
+        [SerializeField] Player p2;
+        public Player P1 { get { return p1; } }
+        public Player P2 { get { return p2; } }
 
 		////Rounds
         [Range(1, 20)] [SerializeField] int noOfRounds;
@@ -31,6 +35,7 @@ namespace CircuitKnights.Controllers
 		[SerializeField] GameObject StartOfMatchCamera;
 		[SerializeField] GameObject StartOfRoundCamera;
 		private bool cutscenePlaying;
+        Animation cameraAnimation;
         // private bool playCutscene;
 		bool skipCutscene = false;
 
@@ -39,93 +44,164 @@ namespace CircuitKnights.Controllers
 		public float ArrivalThreshold = 0.3f;
 		public float ArrivalBrakingFineTuneFactor = 1.25f;
 
-		////GUI
+        [Header("GUI")]
+        [SerializeField] Text roundText;
+        [SerializeField] Text centerText;
 		[SerializeField] GameObject skipButton;
+        private bool isPlaying;
+        private readonly int playerCount = 1;   //Just 2 players
 
-		////Other
+        ////Other
 
         #endregion
 
 
+        void Awake()
+        {
+            ////MORE OVERCOMPLICATIONS!
+            // //Retrieve players using FindObject
+            // var players = Resources.FindObjectsOfTypeAll(typeof(Player)) as Player[];
+            // foreach (var player in players)
+            // {
+            //     if (player.No == Player.Number.One)
+            //         P1 = player;
+            //     else if (player.No == Player.Number.Two)
+            //         P2 = player;
+            //     else
+            //         Debug.LogError("Invalid")
+            // }
+            //Verify that both players were found
+        }
 
 
         void Start()
         {
-			Reset();
-			cutscenePlaying = false;
+            InitializeGame();
+
+            //Run intro
+            StartCoroutine(RunIntro());
+
+            //Begin match
+            BeginNewRound();
+        }
+
+        public static event Action<Player.Number, bool> OnSetPlayerInput = delegate {};
+        public static event Action<Player.Number, bool> OnSetPlayerMovement = delegate {};
+        public static event Action<Player.Number, bool> OnSetPlayerCamera = delegate {};
+
+        private void InitializeGame()
+        {
+            Reset();
+
+            //Disable movement and cameras, but let player move shield and lance for all players
+            SetPlayersInput(true);
+            SetPlayersMovement(false);
+            SetPlayersCamera(false);
+
+            //Hide all GUI
+            roundText.enabled = false;
+            centerText.enabled = false;
+            skipButton.SetActive(false);
+        }
+
+        private IEnumerator RunIntro()
+        {
+            yield return StartCoroutine(RunCutscene(StartOfMatchCamera));
+            yield return StartCoroutine(RunCutscene(StartOfRoundCamera, false));    //Can't be skipped
         }
 
         void Update()
         {
-			//Run start of match cutscene
-			if (Round == 0)
-			{
-				skipCutscene = false;
-            	// cutscenePlaying = RunCutscene();
-			}
-
-            //Begin the match
-            BeginNewRound();
-
-            //Run start of round cutscene
-
+            if (isPlaying)
+            {
+                //
+            }
 
         }
 
-        // private bool RunCutscene(GameObject cutSceneCamera, bool skippable = true)
-        // {
-        //     //Note: Camera must be setup in legacy mode with an animation (NOT ANIMATOR)
 
-        //     ///Initialise
-        //     skipButton.SetActive(false);
-        //     var cameraAnimation = cutSceneCamera.GetComponent<Animation>();
+        private IEnumerator RunCutscene(GameObject cutSceneCamera, bool skippable = true)
+        {
+            //Note: Camera must be setup in legacy mode with an animation (NOT ANIMATOR)
 
-        //     //Set up skipabilitiy
-        //     var skippableTime = Time.time + unskippableDuration;
+            ///Initialise
+            skipButton.SetActive(false);
+            var cameraAnimation = cutSceneCamera.GetComponent<Animation>();
 
-        //     //Set up camera
-        //     cutSceneCamera.SetActive(true);
+            //Set up skipabilitiy
+            var skippableTime = Time.time + unskippableDuration;
 
-        //     //Play the camera
-        //     cameraAnimation.Play();
+            //Set up camera
+            cutSceneCamera.SetActive(true);
+            cameraAnimation.Play();
 
-		// 	//Stop cutscene
+            //Play the camera
+            cutscenePlaying = true;
+            while (cutscenePlaying)
+            {
+                if (skippable)
+                {
+                    if (Time.time >= skippableTime)
+                    {
+                        //Show skip button
+                        skipButton.SetActive(true);
 
-        //     playCutscene = true;
-        //     while (playCutscene)
-        //     {
-        //         if (skippable)
-        //         {
-        //             if (Time.time >= skippableTime)
-        //             {
-        //                 //Show skip button
-        //                 skipButton.SetActive(true);
+                        //Manual skip
+                        if (XCI.GetButtonDown(XboxButton.A, XboxController.First) ||
+                            XCI.GetButtonDown(XboxButton.A, XboxController.Second) ||
+                            Input.GetKeyDown(KeyCode.Space))
+                        {
+                            cutscenePlaying = false;
+                        }
+                    }
+                }
 
-        //                 //Manual skip
-        //                 if (XCI.GetButtonDown(XboxButton.A, XboxController.First) ||
-        //                     XCI.GetButtonDown(XboxButton.A, XboxController.Second) ||
-        //                     Input.GetKeyDown(KeyCode.Space))
-        //                 {
-        //                     playCutscene = false;
-        //                 }
-        //             }
-        //         }
+                //Exit if animation is finished
+                if (!cameraAnimation.isPlaying)
+                    cutscenePlaying = false;
 
-        //         //Exit if animation is finished
-        //         if (!cameraAnimation.isPlaying)
-        //             playCutscene = false;
+                yield return null;
+            }
 
-        //         // yield return null;
-        //     }
+            //Shutdown
+            skipButton.SetActive(false);
+            cutSceneCamera.SetActive(false);
+        }
 
-        //     //Shutdown
-        //     skipButton.SetActive(false);
-        //     cutSceneCamera.SetActive(false);
-        // }
 
         private void BeginNewRound()
         {
+            //Game settings
 			Round++;
+            isPlaying = true;
+
+            //Enable player movement and cameras
+            SetPlayersMovement(true);
+            SetPlayersCamera(true);
+            SetPlayersInput(true);
+        }
+
+
+        void SetPlayersMovement(bool enabled)
+        {
+            for (int i = 0; i < playerCount; i++)
+            {
+                OnSetPlayerMovement((Player.Number)i, enabled);
+            }
+        }
+        void SetPlayersInput(bool enabled)
+        {
+            for (int i = 0; i < playerCount; i++)
+            {
+                OnSetPlayerInput((Player.Number)i, enabled);
+            }
+        }            
+        void SetPlayersCamera(bool enabled)
+        {
+            for (int i = 0; i < playerCount; i++)
+            {
+                OnSetPlayerCamera((Player.Number)i, enabled);
+            }
         }
 
         void Reset()
